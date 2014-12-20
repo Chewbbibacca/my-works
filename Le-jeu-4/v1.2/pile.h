72d3efded21b95b7e82fe1649185e99de013e325
Chewbbibacca <Chewbbibacca@users.noreply.github.com>
2014-12-20 09:47:58 +0100

    Create pile.h

---
+++ Le-jeu-4/v1.2/pile.h
@@ -0,0 +1,9 @@
+#include "header.h"
+
+
+
+void initpile(int numPile);
+void empiler(t_coord coord, int numPile);
+void depiler(t_coord *coord, int numPile);
+int pilevide(int numPile);
+int sommet_pile(t_coord *coord, int numPile);
